97caf3de5362e9f60ba11bd23a9081e6503baf43
unknown <victor_marcantonio@hotmail.com>
2020-09-21 20:18:34 -0300

    inicio implementacao promocao

--- src/main/java/com/mybarber/api/api/dto/promocao/PromocaoDTO.java
+++ src/main/java/com/mybarber/api/api/dto/promocao/PromocaoDTO.java
@@ -1,45 +1,47 @@
 package com.mybarber.api.api.dto.promocao;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class PromocaoDTO {
 
 	private int id;
-	private String nome;
-	private LocalDateTime dataInicio;
-	private LocalDateTime dataFim;
+	private Date dataInicio;
+	private Date dataFim;
+	private String descricao;
+	private Long idServico;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public LocalDateTime getDataInicio() {
+	public Date getDataInicio() {
 		return dataInicio;
 	}
-	public void setDataInicio(LocalDateTime dataInicio) {
+	public void setDataInicio(Date dataInicio) {
 		this.dataInicio = dataInicio;
 	}
-	public LocalDateTime getDataFim() {
+	public Date getDataFim() {
 		return dataFim;
 	}
-	public void setDataFim(LocalDateTime dataFim) {
+	public void setDataFim(Date dataFim) {
 		this.dataFim = dataFim;
 	}
-	public PromocaoDTO(int id, String nome, LocalDateTime dataInicio, LocalDateTime dataFim) {
-		this.id = id;
-		this.nome = nome;
-		this.dataInicio = dataInicio;
-		this.dataFim = dataFim;
+	public String getDescricao() {
+		return descricao;
 	}
-	
-	
-	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Long getIdServico() {
+		return idServico;
+	}
+	public void setIdServico(Long idServico) {
+		this.idServico = idServico;
+	}
+	public PromocaoDTO() {
+	}
+   
 	
 }
