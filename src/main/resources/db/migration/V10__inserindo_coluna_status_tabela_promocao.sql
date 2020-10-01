4bf6ffbb0faa142e9785d21432f901e37f8aaf7b
Julio Cesar <julioosilva97@gmail.com>
2020-10-01 20:31:35 -0300

    flyway do carai

--- src/main/resources/db/migration/V10__inserindo_coluna_status_tabela_promocao.sql
+++ src/main/resources/db/migration/V10__inserindo_coluna_status_tabela_promocao.sql
@@ -1,2 +1,9 @@
-ALTER TABLE promocao
-ADD COLUMN status boolean;
+DO $$ 
+    BEGIN
+        BEGIN
+        ALTER TABLE promocao ADD COLUMN status boolean;
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
+        END;
+    END;
+$$
